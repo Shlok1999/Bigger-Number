f217bdb40ad8f874f2202a6046969b727dabf42c
shlok1999 <09shlok1999ae1@gmail.com>
2021-12-17 23:07:57 +0530

    Files commited

---
+++ app/src/main/java/com/example/biggernumber/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.biggernumber
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        playGame()
+    }
+    fun playGame(){
+        //Left Button Clicked
+        btnLeft.setOnClickListener {
+            val leftNum = btnLeft.text.toString().toInt()
+            val rightNum = btnRight.text.toString().toInt()
+            if(leftNum > rightNum ){
+                //Change of background color
+                gameInterface.setBackgroundColor(Color.GREEN)
+                Toast.makeText(this, "Correct Answer", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show()
+                //Change of background color
+                gameInterface.setBackgroundColor(Color.RED)
+            }
+            reStart()
+        }
+        //Right Button Clicked
+        btnRight.setOnClickListener {
+            val leftNum = btnLeft.text.toString().toInt()
+            val rightNum = btnRight.text.toString().toInt()
+            if(leftNum < rightNum ){
+                //Change of background color
+                gameInterface.setBackgroundColor(Color.GREEN)
+                Toast.makeText(this, "Correct Answer", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show()
+                //Change of background color
+                gameInterface.setBackgroundColor(Color.RED)
+            }
+            reStart()
+        }
+    }
+    fun reStart(){
+        val randomValue1 = Random.nextInt(1,10)
+        val randomValue2 = Random.nextInt(1,10)
+        btnLeft.text = randomValue1.toString()
+        btnRight.text = randomValue2.toString()
+    }
+}
